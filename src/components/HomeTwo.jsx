7fc41f9d7f8bd4d36150e837716440574bc704cd
Dztinct <detunjinov@gmail.com>
2025-04-26 13:23:40 +0100

    ready for hosting-little adjustments

--- src/components/HomeTwo.jsx
+++ src/components/HomeTwo.jsx
@@ -3,7 +3,7 @@ import box from '../images/love-box.jpg'
 const HomeTwo = () => {
   return (
     <div>
-      <div className="m-0 p-0 min-h-screen bg-white flex justify-center items-center">
+      <div className="my-0 py-0 px-0 mx-0 min-h-screen bg-white flex justify-center items-center">
         <div className="container flex justify-center">
           <div className="max-w-sm">
             <div className="bg-red-700 relative shadow-lg hover:shadow-xl transition duration-500">
@@ -13,9 +13,6 @@ const HomeTwo = () => {
                 <p className="text-gray-100 tracking-wide text-center">Send a WhatsApp message through the link below.</p>
                 <a href="https://wa.me/message/N7RLKYVMOWZSN1" className="mt-6 py-2 px-4 block bg-white text-red-700 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300 text-center">Click here</a>
               </div>
-              {/* <div className="absolute top-2 right-2 py-2 px-4 bg-white rounded-lg">
-                <span className="text-md">$150</span>
-            </div> */}
             </div>
           </div>
         </div>
